228dbfab721a6e7e35d1ba5bb788d08429a86721
ammar999-nightowl <mdammarali.4888@gmail.com>
2026-09-25 17:50:17 +0530

    added file saving (complete v3)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-//wanna add filesave later todo:
+// wanna add filesave later
 #include <fstream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    // 1. Declare variables inside main so they are accessible throughout the function
-    string name;           // employee name
-    string company_name;   // company name
-    string info_employee;  // info of employee
-    string info_company;   // info of company
-    string role_employee;  // role of employee
-    
-    int expirience;        // years of experience
-    int age_employee;      // age of employee
+    // 1. Declare variables inside main so they are accessible throughout the
+    // function
+    string name;          // employee name
+    string company_name;  // company name
+    string info_employee; // info of employee
+    string info_company;  // info of company
+    string role_employee; // role of employee
+
+    int expirience;   // years of experience
+    int age_employee; // age of employee
 
     // 2. Gather Inputs (using getline for full sentences/spaces)
     cout << "Name of employee : ";
@@ -41,14 +42,50 @@ int main() {
     // 3. Print Output ID Card
     cout << "\n__________EMPLOYEE ID__________" << endl;
     cout << "Name             : " << name << endl;
-    cout << "Name of Company  : " << company_name << endl; // Fixed missing company_name variable
+    cout << "Name of Company  : " << company_name
+         << endl; // Fixed missing company_name variable
     cout << "Info             : " << info_employee << endl;
     cout << "Company Info     : " << info_company << endl;
     cout << "Role             : " << role_employee << endl;
     cout << "Age              : " << age_employee << endl;
     cout << "Experience Years : " << expirience << endl;
-    cout << "_______________________________" << endl; // Added missing semicolon
+    cout << "_______________________________"
+         << endl; // Added missing semicolon
+    // lets set file saving mechanism
+    char Savechoice;
+    cout << "would you like to save the id as file";
+     cin >> Savechoice;
+    // now setup if else system for the Savechoice variable
+    if (Savechoice == 'y' || 'Y') {
+        // now setup the main magic
+        //  the file saving
+        ofstream outFile("generated_id.txt");
 
-    return 0;
+        if (outFile.is_open()) {
+            outFile << "employee id\n";
+            outFile << "name :" << name << "\n";
+            outFile << "company name :" << company_name << "\n";
+            outFile << "info :" << info_employee << "\n";
+            outFile << "company info :" << info_company << "\n";
+            outFile << "Role :" << role_employee << "\n";
+            outFile << "Age :" << age_employee << "\n";
+            outFile << "Experience years :" << expirience << "\n";
+            outFile << "________________________" << "\n";
+
+            outFile.close();
+            cout << "employee id saved maybe" << endl;
+        } else {
+            cout << "oh no looks like a error is here" << endl;
+        }
+    } else if (Savechoice == 'n' || Savechoice == 'N') {
+        cout << "file isnt saved" << endl;
+    }
+  return 0;
 }
-//i used some ai help. because original code had errors. the code is 100% genuine human made 
+
+
+// i used some ai help. because original code had errors. the code is 100%
+// genuine human made
+//now fixed some errors myself
+// because i am not a ai slop dev
+// i am a genuine dev
